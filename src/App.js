24859f38ec2acf9457f4dcc713387991e95b846c
doyeon <doyeon-themyplanet@github.com>
2021-12-27 14:21:06 +0900

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Hot_topic_writer, Hot_topic_title, Hot_topic_contents, Post_contents_title, Post_contents } from './data';
 
 function App() {
+  return (
+    <>
+      <Header__top />
+      <Header__botton />
+      <Blog__top />
+      <Blog__bottom />
+    </>
+  );
+}
+
+function Header__top() {
   return (
     <>
       <div className="header__top">
@@ -9,9 +21,9 @@ function App() {
           <div className="header__top-left">
             <div className="header__logo">
               <a href="#" className="logo__link">
-                <img className="logo-img" src="./images/logo.svg" alt="logo" /> 
-                <img className="blog-logo-img" src="./images/blog.svg" alt="blog" /> 
-                <img className="life-logo-img" src="./images/lifeLogo.svg" alt="lifeLogo" /> 
+                <img className="logo-img" src="./images/logo.svg" alt="logo" />
+                <img className="blog-logo-img" src="./images/blog.svg" alt="blog" />
+                <img className="life-logo-img" src="./images/lifeLogo.svg" alt="lifeLogo" />
               </a>
             </div>
 
@@ -24,9 +36,9 @@ function App() {
                   </div>
 
                   <ul className="select__list">
-                      <li className="text active">글</li>
-                      <li className="blog">블로그</li>
-                      <li className="nickname">별명 아이디</li>
+                    <li className="text active">글</li>
+                    <li className="blog">블로그</li>
+                    <li className="nickname">별명 아이디</li>
                   </ul>
                 </div>
 
@@ -36,7 +48,9 @@ function App() {
                 </div>
 
                 <div className="search__button">
-                  <button><img src="./images/search-button.png" alt="search_button" /></button>
+                  <button>
+                    <img src="./images/search-button.png" alt="search_button" />
+                  </button>
                 </div>
               </form>
 
@@ -45,12 +59,13 @@ function App() {
               </div>
             </div>
           </div>
-
           <div className="header__top-right">
             <div className="header__auth">
               <div className="auth__user">
                 <span className="auth__user-icon"></span>
-                <span><span className="auth__user-name">luke</span> ▾</span>
+                <span>
+                  <span className="auth__user-name">luke</span> ▾
+                </span>
               </div>
 
               <div className="auth__alarm">
@@ -69,35 +84,78 @@ function App() {
           </div>
         </div>
       </div>
+    </>
+  );
+}
 
+function Header__botton() {
+  return (
+    <>
       <div className="header__bottom">
         <div className="confix">
           <nav>
             <ul>
-              <li><a className="active" href="https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0">블로그 홈</a></li>
-              <li><a href="https://section.blog.naver.com/ThemePost.naver?directoryNo=0&activeDirectorySeq=0&currentPage=1">주제별 보기</a></li>
-              <li><span></span></li>
-              <li><a href="https://section.blog.naver.com/ThisMonthDirectory.naver">이달의 블로그</a></li>
-              <li><a href="https://section.blog.naver.com/OfficialBlog.naver?currentPage=1">공식블로그</a></li>
-              <li><a href="https://section.blog.naver.com/HotTopicChallenge.naver?currentPage=1">챌린지 프로그램</a></li>
+              <li>
+                <a
+                  className="active"
+                  href="https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0">
+                  블로그 홈
+                </a>
+              </li>
+              <li>
+                <a href="https://section.blog.naver.com/ThemePost.naver?directoryNo=0&activeDirectorySeq=0&currentPage=1">
+                  주제별 보기
+                </a>
+              </li>
+              <li>
+                <span></span>
+              </li>
+              <li>
+                <a href="https://section.blog.naver.com/ThisMonthDirectory.naver">이달의 블로그</a>
+              </li>
+              <li>
+                <a href="https://section.blog.naver.com/OfficialBlog.naver?currentPage=1">공식블로그</a>
+              </li>
+              <li>
+                <a href="https://section.blog.naver.com/HotTopicChallenge.naver?currentPage=1">챌린지 프로그램</a>
+              </li>
             </ul>
           </nav>
 
           <div className="blog-info-link">
             <ul>
-              <li><a href="https://seller.blog.naver.com/market/joinGuide">블로그 마켓 가입</a></li>
-              <li><a href="http://item2.naver.com/" target="_blank">아이템 팩토리</a></li>
-              <li><a href="https://blog.naver.com/blogpeople" target="_blank">블로그팀 공식 블로그</a></li>
+              <li>
+                <a href="https://seller.blog.naver.com/market/joinGuide">블로그 마켓 가입</a>
+              </li>
+              <li>
+                <a href="http://item2.naver.com/" target="_blank">
+                  아이템 팩토리
+                </a>
+              </li>
+              <li>
+                <a href="https://blog.naver.com/blogpeople" target="_blank">
+                  블로그팀 공식 블로그
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
+    </>
+  );
+}
 
+function Blog__top() {
+  return (
+    <>
       <div className="blog-top">
         <div className="confix">
           <div className="hot-topic">
             <div className="hot-topic__select">
-              <h2><span className="hot-topic__main-title">핫토픽</span>&nbsp;<span className="hot-topic__arrow"> &gt; </span>&nbsp;집밥</h2>
+              <h2>
+                <span className="hot-topic__main-title">핫토픽</span>&nbsp;<span className="hot-topic__arrow"> &gt; </span>
+                &nbsp;집밥
+              </h2>
               <div className="hot-topic__num">
                 <span className="active">1</span>
                 <span>2</span>
@@ -112,47 +170,47 @@ function App() {
                 <li>
                   <img src="./images/hot-topic-rice1.jpg" alt="hot-topic-rice1" />
                   <div className="list__text">
-                    <p className="list__text-title">4인 가족 식비 절약 집밥 기록~</p>
+                    <p className="list__text-title">{Hot_topic_title(0)}</p>
 
                     <div className="list__hide-text">
                       <div className="post-icon">
                         <span className="post-icon-img"></span>
-                        <span className="post-icon-name">부자엄마</span>
+                        <span className="post-icon-name">{Hot_topic_writer(0)}</span>
                       </div>
 
-                      <p>요즘 한 주 한 주 집밥 기록을 하고 있다. 일주일 동안 뭘 먹었는지 확인하고 영양적으로 식비 쪽으로 부족하거나 과한 건 없는지...</p>
-                    </div> 
-                  </div> 
+                      <p>{Hot_topic_contents(0)}</p>
+                    </div>
+                  </div>
                 </li>
                 <li>
                   <img src="./images/hot-topic-rice2.jpg" alt="hot-topic-rice2" />
                   <div className="list__text">
-                    <p className="list__text-title">애호박 무 넣은 된장찌개 끓이기</p>
+                    <p className="list__text-title">{Hot_topic_title(1)}</p>
 
                     <div className="list__hide-text">
                       <div className="post-icon">
                         <span className="post-icon-img"></span>
-                        <span className="post-icon-name">수동샘</span>
+                        <span className="post-icon-name">{Hot_topic_writer(1)}</span>
                       </div>
 
-                      <p>돌솥비빔밥과 매콤소고기뭇국 햄 감자볶음, 감자조림, 메추리알 장조림 우리 부부는 햄을 안 좋아하는데 명절에 들어온 햄 처리용...</p>
-                    </div> 
+                      <p>{Hot_topic_contents(1)}</p>
+                    </div>
                   </div>
                 </li>
                 <li>
                   <img src="./images/hot-topic-rice3.jpg" alt="hot-topic-rice3" />
                   <div className="list__text">
-                    <p className="list__text-title">나의 자취 집밥 기록~</p>
+                    <p className="list__text-title">{Hot_topic_title(2)}</p>
 
                     <div className="list__hide-text">
                       <div className="post-icon">
                         <span className="post-icon-img"></span>
-                        <span className="post-icon-name">아브</span>
+                        <span className="post-icon-name">{Hot_topic_writer(2)}</span>
                       </div>
 
-                      <p>#집밥기록 나의 집밥 기록 단배추 된장 비빔밥과 진미채무침, 그리고 단백질 보충용 삶은 달걀 단배추 된장 비빔밥은 정말 맛...</p>
-                    </div> 
-                  </div> 
+                      <p>{Hot_topic_contents(2)}</p>
+                    </div>
+                  </div>
                 </li>
               </ul>
             </div>
@@ -166,12 +224,20 @@ function App() {
             </div>
 
             <div className="ad__img">
-              <a href="https://blog.naver.com/blogpeople/222599846369" target="_blank"><img src="./images/ad-1.jpg" alt="ad1" /></a>
+              <a href="https://blog.naver.com/blogpeople/222599846369" target="_blank">
+                <img src="./images/ad-1.jpg" alt="ad1" />
+              </a>
             </div>
           </div>
         </div>
       </div>
+    </>
+  );
+}
 
+function Blog__bottom() {
+  return (
+    <>
       <div className="blog-bottom">
         <div className="confix">
           <div className="neighbor-post">
@@ -180,20 +246,26 @@ function App() {
               <ul className="neighbor-select">
                 <li>
                   <span>전체이웃</span>
-                  <span><i className="fas fa-chevron-down"></i></span>
+                  <span>
+                    <i className="fas fa-chevron-down"></i>
+                  </span>
                 </li>
               </ul>
-            </div> 
+            </div>
 
             <div className="neighbor-filter__bottom">
               <ul className="neighbor-select">
                 <li>
-                  <span>내 이웃 <span className="neighbor-name">돌</span>님의 이웃을 만나보세요.</span>
-                  <span><i className="fas fa-chevron-down"></i></span>
+                  <span>
+                    내 이웃 <span className="neighbor-name">돌</span>님의 이웃을 만나보세요.
+                  </span>
+                  <span>
+                    <i className="fas fa-chevron-down"></i>
+                  </span>
                 </li>
               </ul>
             </div>
-            
+
             <ul className="post-list">
               <li>
                 <div className="neighbor-post__text">
@@ -207,8 +279,8 @@ function App() {
                   </div>
 
                   <div className="post-contents">
-                    <h3>눈이 내리는 밤</h3>
-                    <p>서울에서 내려와 세종에 도착 버스에 내렸을 때 눈이 내리고 있었다. 눈 내리는 밤 도로가에 눈이 소복소복 쌓여간다. 그리고 다음날 아이들과 눈 사람 만들기</p>
+                    <h3> {Post_contents_title(0)} </h3>
+                    <p> {Post_contents(0)} </p>
 
                     <div className="comment">
                       <div className="comment-left">
@@ -216,22 +288,24 @@ function App() {
                           <div className="heart-img"></div>
                           <span>공감해요</span>
                           <span className="comment__heart-num">4</span>
-                        </div> 
+                        </div>
 
                         <div className="comment__comment">
-                          <div className="comment-img"><i className="fas fa-comment-dots"></i></div>
+                          <div className="comment-img">
+                            <i className="fas fa-comment-dots"></i>
+                          </div>
                           <span className="comment__comment-num">0</span>
-                        </div> 
-                      </div> 
-                      
+                        </div>
+                      </div>
+
                       <div className="comment-right">
                         <div className="comment__share">
                           <i className="far fa-share-square"></i>
                         </div>
-                      </div> 
+                      </div>
                     </div>
-                  </div> 
-                </div> 
+                  </div>
+                </div>
 
                 <div className="neighbor-post__img">
                   <img src="./images/post-img.png" alt="post-img" />
@@ -249,8 +323,9 @@ function App() {
                   </div>
 
                   <div className="post-contents">
-                    <h3>리액트 ES6 화살표 함수</h3>
-                    <p>화살표 함수 (arrow function)이라고 한다. ES6 추가된 표현식을 사용하는 함수로 화살표 기호 => 이 소스는 책에서 Github에 있는 소스 위 소스 중 대표적인 화살표 함수 위 화살표 함수 표현식이 이 것과 같다. 훨씬 간결하고 편하게 쓰여지는 ES6 문법의 화살표 함수</p>
+                    <h3> {Post_contents_title(1)} </h3>
+                    <p> {Post_contents(1)} </p>
+
                     <div className="comment">
                       <div className="comment-left">
                         <div className="comment__heart">
@@ -260,19 +335,21 @@ function App() {
                         </div>
 
                         <div className="comment__comment">
-                          <div className="comment-img"><i className="fas fa-comment-dots"></i></div>
+                          <div className="comment-img">
+                            <i className="fas fa-comment-dots"></i>
+                          </div>
                           <span className="comment__comment-num">0</span>
                         </div>
                       </div>
-                      
+
                       <div className="comment-right">
                         <div className="comment__share">
                           <i className="far fa-share-square"></i>
                         </div>
-                      </div> 
+                      </div>
                     </div>
-                  </div> 
-                </div> 
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
@@ -287,7 +364,9 @@ function App() {
                 </div>
 
                 <div className="user-visit">
-                  <span>오늘 <span className="visit-num">0</span>명 방문 &gt;</span>
+                  <span>
+                    오늘 <span className="visit-num">0</span>명 방문 &gt;
+                  </span>
                 </div>
               </div>
 
@@ -298,7 +377,12 @@ function App() {
 
             <div className="my-blog__link">
               <button className="blog-button">내 블로그</button>
-              <button className="write-button"><span className="write-icon"><i className="fas fa-pen"></i></span> 글쓰기</button>
+              <button className="write-button">
+                <span className="write-icon">
+                  <i className="fas fa-pen"></i>
+                </span>{' '}
+                글쓰기
+              </button>
             </div>
 
             <div className="my-blog__list">
@@ -325,13 +409,16 @@ function App() {
                   <span>새로운 소식이 없습니다.</span>
                 </li>
               </ul>
-            </div> 
+            </div>
 
             <div className="from-blog">
               <div className="from-blog-text">
                 <span>From, 블로그씨</span>
-                <p>블로그씨는 곧 있을 크리스마스를 위해 미니 트리를 만들었어요~ 내가 만든 소품 DIY를 사진과 함께 공유해 주세요!</p>
-              </div> 
+                <p>
+                  블로그씨는 곧 있을 크리스마스를 위해 미니 트리를 만들었어요~ 내가 만든 소품 DIY를 사진과 함께 공유해
+                  주세요!
+                </p>
+              </div>
 
               <div className="from-blog-bottom">
                 <div className="question">
@@ -341,7 +428,7 @@ function App() {
                 <div className="setting">
                   <i className="fas fa-cog"></i>
                 </div>
-              </div> 
+              </div>
             </div>
 
             <div className="move-btn">
@@ -349,7 +436,7 @@ function App() {
               <span>&gt;</span>
             </div>
           </div>
-        </div> 
+        </div>
       </div>
     </>
   );
